dc77dace81b8479b6fe21fbd278f8108c8cf339d
JrDeleyFerreira <deleyalemao@gmail.com>
2024-03-12 02:05:03 -0300

    Seção 1: Back-end da aplicação (C#).

--- Back/src/ProEventos.Api/Program.cs
+++ Back/src/ProEventos.Api/Program.cs
@@ -1,9 +1,33 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers()
+    .AddJsonOptions
+    (
+        options =>
+        {
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        }
+    );
+
+// Declarando contexto de base de dados
+builder.Services.AddDbContext<ProEventosContext>(
+    options => options.UseSqlite(builder.Configuration.GetConnectionString("Default"))
+);
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Dependence Injections
+builder.Services.AddScoped<IBasePersistence, BasePersistence>();
+builder.Services.AddScoped<IEventoPersistence, EventoPersistence>();
+builder.Services.AddScoped<IEventoService, EventoService>();
+
+// AutoMappers
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -14,6 +38,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+
+app.UseCors(x => x
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowAnyOrigin());
 
 app.Run();
